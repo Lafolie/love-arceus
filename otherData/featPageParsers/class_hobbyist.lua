836816a4bfa3bf35897e69de5238fcf1e026d646
Lafolie <lafoliex7@aol.com>
2020-03-14 23:53:34 +0000

    completed Hobbyist class parser

--- otherData/featPageParsers/class_hobbyist.lua
+++ otherData/featPageParsers/class_hobbyist.lua
@@ -2,78 +2,48 @@
 local parser = require "otherData.featParser"
 local names = 
 {
-	"Ace Trainer",
-	"Perseverance",
-	"Elite Trainer",
-	"Critical Moment",
-	"Top Percentage",
-	"Signature Technique",
-	"Champ in the Making"
+	"Hobbyist",
+	"Dilettante",
+	"Dabbler",
+	"Look and Learn"
 }
 
 local m = string.match
 
 local function strip(parser, str)
 	-- local firstFeat = parser.names[1]
-	return m(str, "(.-)Cone, Line, Burst, and Blast Moves\r\n")
+	return m(str, "(.-)Scene Features List Action Point Features List\r\n")
 end
 
 local function post(feats, str)
 	local ex = {}
-	local techMods = {}
-	local headers = 
-	{
-		"Cone, Line, Burst, and Blast Moves",
-		"Single Target Moves",
-		"Damaging Moves",
-		"Status Moves",
-		"$"
-	}
+	-- local exStr = m(str, "Trainer Class Feature Prerequisite Trainer Class Feature Prerequisite\r\n(.+)")
+	
+	local sceneFeats = {}
+	local actionFeats = {}
 
-	local names =
-	{
-		{"Scattershot", "Shock and Awe", "Vicious Storm", "$"},
-		{"Guarding Strike", "Unbalancing Blow", "Reliable Attack", "$"},
-		{"Alternative Energy", "Bloodied Speed", "Double Down", "$"},
-		{"Burst of Motivation", "Supreme Concentration", "Double Curse", "$"}
-	}
-
-	str = m(str, "(Cone, Line, Burst, and Blast Moves.+)Signature Technique Modifications\r\n")
-
-	for n = 1, #headers - 1 do
-		local modCat = {}
-		local start = headers[n]
-		local finish = headers[n + 1]
-		local modNames = names[n]
-		-- split string into 4 header chunks
-		local chunkStr = m(str, start .. "(.-)" .. finish)
-		
-		modCat.name = start
-		modCat.mods = {}
-
-		-- iterate over names
-		for i = 1, 3 do
-			local mod = {}
-			local ptn = string.format("(%s.-)%s", modNames[i], modNames[i + 1])
-			local modStr = m(chunkStr, ptn)
-
-			-- break apart
-			local name, training, fx = m(modStr, "(.-) %- (.-):%s*(.+)")
-			fx = string.gsub(fx, "\r\n", " ")
-			fx = m(fx, "%s*(.+%S)")
-
-			mod.name = name
-			mod.training = training
-			mod.fx = fx
-
-			table.insert(modCat.mods, mod)
-		end
-
-		table.insert(techMods, modCat)
+	local function makeFeat(tbl, class, name, preqs)
+		table.insert(tbl, {name = name, class = class, preqs = preqs})
 	end
 
-	ex.techMods = techMods
+	-- fuck this grid shit, do it manually
+	makeFeat(sceneFeats, "Ace Trainer", "Critical Moment", "Commander's Voice")
+	makeFeat(sceneFeats, "Capture Specialist", "Capture Techniques (False Strike and Catch Combo Only", "Poké Ball Crafter")
+	makeFeat(sceneFeats, "Enduring Soul", "Staying Power", "Medic Training")
+	makeFeat(sceneFeats, "Trickster", "Sleight", "Command Versatility")
+	makeFeat(sceneFeats, "Researcher", "Chemical Warfare", "Repel Crafter")
+	makeFeat(sceneFeats, "Roughneck", "Mettle", "Defender")
+
+	makeFeat(actionFeats, "Coordinator", "Nuanced Performance", "Grace")
+	makeFeat(actionFeats, "Juggler", "Round Trip", "Quick Switch")
+	makeFeat(actionFeats, "Chef", "Hits the Spot", "Basic Cooking")
+	makeFeat(actionFeats, "Fashionista", "Style is Eternal", "Groomer")
+	makeFeat(actionFeats, "Athlete", "Coaching", "Swimmer")
+	makeFeat(actionFeats, "Tumbler", "Quick Gymnastics", "Acrobat")
+
+	ex.sceneFeats = sceneFeats
+	ex.actionFeats = actionFeats
 	return ex
 end
 
-return parser("aceTrainer.txt", "Ace Trainer Class", names, strip, post)
+return parser("hobbyist.txt", "Hobbyist Class", names, strip, post)
