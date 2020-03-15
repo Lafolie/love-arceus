3f9c215eef8e2c1f6eebf02fc5fd9d4d7a959d62
Lafolie <lafoliex7@aol.com>
2020-03-15 00:01:49 +0000

    fixed incorrect mentor page rips

--- otherData/ripOtherData.ps1
+++ otherData/ripOtherData.ps1
@@ -108,7 +108,7 @@ $app = "E:\ChromeDownloads\xpdf-tools-win-4.02\xpdf-tools-win-4.02\bin64\pdftote
 .$app -f 81 -l 81 -raw $core "E:\Python\love-arceus\otherData\core\features\commander.txt"
 .$app -f 83 -l 84 -raw $core "E:\Python\love-arceus\otherData\core\features\coordinator.txt"
 .$app -f 86 -l 86 -raw $core "E:\Python\love-arceus\otherData\core\features\hobbyist.txt"
-.$app -f 87 -l 88 -raw $core "E:\Python\love-arceus\otherData\core\features\mentor.txt"
+.$app -f 88 -l 89 -raw $core "E:\Python\love-arceus\otherData\core\features\mentor.txt"
 .$app -f 93 -l 94 -raw $core "E:\Python\love-arceus\otherData\core\features\cheerleader.txt"
 .$app -f 99 -l 99 -raw $core "E:\Python\love-arceus\otherData\core\features\enduringSoul.txt"
 .$app -f 101 -l 101 -raw $core "E:\Python\love-arceus\otherData\core\features\juggler.txt"
